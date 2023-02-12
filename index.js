8349124e49094b2be09f1c7a712509a4170ce8da
Hirokazu Hirono <hirono.hirokazu@gmail.com>
2023-02-12 22:31:53 +0900

    Add javascript

--- index.js
+++ index.js
@@ -1,49 +1,42 @@
 const resultDialog = document.getElementById('resultDialog');
 
 const firstName = document.getElementById('firstName');
-const firstNameErrorImg = firstName.nextElementSibling;
-const firstNameErrorMsg = firstName.parentElement.nextElementSibling;
-
 const lastName = document.getElementById('lastName');
-const lastNameErrorImg = lastName.nextElementSibling;
-const lastNameErrorMsg = lastName.parentElement.nextElementSibling;
-
 const email = document.getElementById('email');
 const emailErrorImg = email.nextElementSibling;
 const emailErrorMsg = email.parentElement.nextElementSibling;
-
 const password = document.getElementById('password');
-const passwordErrorImg = password.nextElementSibling;
-const passwordErrorMsg = password.parentElement.nextElementSibling;
-
 const form = document.getElementById('form');
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
-  if (firstName.value.length === 0) {
-    firstName.classList.add('input-error');
-    firstNameErrorImg.style.visibility = 'visible';
-    firstNameErrorMsg.style.visibility = 'visible';
-  }
-  if (lastName.value.length === 0) {
-    lastName.classList.add('input-error');
-    lastNameErrorImg.style.visibility = 'visible';
-    lastNameErrorMsg.style.visibility = 'visible';
-  }
+  const elArr = [firstName, lastName, password];
+  elArr.forEach((el) => {
+    if (el.value.trim().length === 0) {
+      el.classList.add('input-error');
+      let elErrorImg = el.nextElementSibling;
+      let elErrorMsg = el.parentElement.nextElementSibling;
 
+      elErrorImg.style.visibility = 'visible';
+      elErrorMsg.style.visibility = 'visible';
+    } else {
+      el.classList.remove('input-error');
+      let elErrorImg = el.nextElementSibling;
+      let elErrorMsg = el.parentElement.nextElementSibling;
+
+      elErrorImg.style.visibility = 'hidden';
+      elErrorMsg.style.visibility = 'hidden';
+    }
+  });
   if (!validateEmail(email.value)) {
     email.classList.add('input-error');
     emailErrorImg.style.visibility = 'visible';
     emailErrorMsg.style.visibility = 'visible';
+  } else {
+    email.classList.remove('input-error');
+    emailErrorImg.style.visibility = 'hidden';
+    emailErrorMsg.style.visibility = 'hidden';
   }
-
-  if (password.value.length === 0) {
-    password.classList.add('input-error');
-    passwordErrorImg.style.visibility = 'visible';
-    passwordErrorMsg.style.visibility = 'visible';
-  }
-
-  resultDialog.showModal();
 });
 
 function validateEmail(email) {
